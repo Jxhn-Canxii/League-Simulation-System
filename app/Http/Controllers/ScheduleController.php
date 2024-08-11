0eb7e99c989e95421c61e6061ee687b3ecedd9fe
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2024-08-11 09:14:58 +0800

    Update Injury Percentage Calculation

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -131,11 +131,12 @@ class ScheduleController extends Controller
             }
 
            // Determine if the player should have an injury_prone_percentage of 0
-            if (rand(1, 100) <= 80) {
-                $player->injury_prone_percentage = 0;
-            } else {
+            if (rand(1, 100) <= 50) {
                 // Assign a random value between 1 and 100
                 $player->injury_prone_percentage = rand(1, 100);
+            } else {
+
+                $player->injury_prone_percentage = 0;
             }
 
 
