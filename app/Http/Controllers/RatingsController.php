1457073a9b4e7166244ea4ec446a3eb37b96d932
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2024-10-28 23:43:30 +0800

    Update RatingsController.php

--- app/Http/Controllers/RatingsController.php
+++ app/Http/Controllers/RatingsController.php
@@ -213,27 +213,30 @@ class RatingsController extends Controller
 
                 // Log the updated ratings
                 $this->logPlayerRatings($player, $seasonId);
+
+                     // Select the 5 players with the worst statistics and update them
+                    $worstPlayers = $rankedPlayers->slice(-3);
+
+                    foreach ($worstPlayers as $playerStat) {
+
+                        // Log the transaction for the waived player
+                        DB::table('transactions')->insert([
+                            'player_id' => $playerStat->player_id,
+                            'season_id' => $seasonId,
+                            'details' => 'Waived by ' . ($teamName ?? 'Unknown Team'),
+                            'from_team_id' => $teamId,
+                            'to_team_id' => 0,
+                            'status' => 'waived',
+                        ]);
+
+                        Player::where('id', $playerStat->player_id)->update([
+                            'contract_years' => 0,
+                            'team_id' => 0
+                        ]);
+                    }
             }
 
-            // Select the 3 players with the worst statistics and update them
-            $worstPlayers = $rankedPlayers->slice(rand(-3,-4));
 
-            foreach ($worstPlayers as $playerStat) {
-                Player::where('id', $playerStat->player_id)->update([
-                    'contract_years' => 0,
-                    'team_id' => 0
-                ]);
-
-                // Log the transaction for the waived player
-                DB::table('transactions')->insert([
-                    'player_id' => $playerStat->player_id,
-                    'season_id' => $seasonId,
-                    'details' => 'Waived by ' . ($teamName ?? 'Unknown Team'),
-                    'from_team_id' => $teamId,
-                    'to_team_id' => 0,
-                    'status' => 'waived',
-                ]);
-            }
             // Fetch the team name if team_id is provided
             $teamName = '';
             if ($teamId) {
