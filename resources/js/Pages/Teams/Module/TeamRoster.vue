3c9f8326c48f41a688051c5aaa4e38704a999f12
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2024-10-28 21:07:49 +0800

    Update TeamRoster.vue

--- resources/js/Pages/Teams/Module/TeamRoster.vue
+++ resources/js/Pages/Teams/Module/TeamRoster.vue
@@ -130,7 +130,7 @@
                         <td class="px-2 py-1 whitespace-nowrap border">
                             {{ index + 1 }}
                         </td>
-                        <td class="px-2 py-1 whitespace-nowrap border" :title="'Draft class'+player.draft_class">
+                        <td class="px-2 py-1 whitespace-nowrap border" :title="'Draft class: '+player.draft_class">
                             {{ player.draft_status }} {{ player.drafted_team ? '('+player.drafted_team+ ')' : ''}}
                         </td>
                         <td class="px-2 py-1 whitespace-nowrap border">
