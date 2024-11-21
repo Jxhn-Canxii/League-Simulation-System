4f65362a6103135283634f5cbe143e8527df1017
Jxhn-Canxii <56583469+Jxhn-Canxii@users.noreply.github.com>
2024-11-21 16:24:37 +0800

    Update RatingsController.php

--- app/Http/Controllers/RatingsController.php
+++ app/Http/Controllers/RatingsController.php
@@ -236,10 +236,10 @@ class RatingsController extends Controller
                 }
 
                 // Determine if the player should have an injury_prone_percentage of 0
-                if (rand(1, 100) <= 20) {
+                if (rand(1, 100) <= 30) {
                     // 40% chance to be injury-prone
                     // Assign a random value between 10 and 100 in increments of 10
-                    $player->injury_prone_percentage = rand(0, 100);
+                    $player->injury_prone_percentage = rand(50, 100);
                 } else {
                     $player->injury_prone_percentage = 0;
                 }
