09514459008f4457ffb3f921a1cf3c112d6135fa
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2024-11-29 06:49:00 +0800

    Update RatingsController.php

--- app/Http/Controllers/RatingsController.php
+++ app/Http/Controllers/RatingsController.php
@@ -357,16 +357,16 @@ class RatingsController extends Controller
                     ]);
 
                 ///active players with teams
-                DB::table('players')
-                    ->where('team_id', '>', 0)
-                    ->where('is_active', 1)
-                    ->update([
-                        'is_rookie' => 0,
-                        'age' => DB::raw('age + 1'), // Increment age by 1
-                        'contract_years' => DB::raw("CASE WHEN age + 1 >= retirement_age THEN 0 ELSE contract_years END"), // Set contract_years to 0 if age reaches retirement_age
-                        'team_id' => DB::raw("CASE WHEN age + 1 >= retirement_age THEN 0 ELSE team_id END"), // Set team_id to 0 if age reaches retirement_age
-                        'is_active' => DB::raw("CASE WHEN age + 1 >= retirement_age THEN 0 ELSE is_active END"), // Set is_active to 0 if age reaches retirement_age
-                    ]);
+                // DB::table('players')
+                //     ->where('team_id', '>', 0)
+                //     ->where('is_active', 1)
+                //     ->update([
+                //         'is_rookie' => 0,
+                //         'age' => DB::raw('age + 1'), // Increment age by 1
+                //         'contract_years' => DB::raw("CASE WHEN age + 1 >= retirement_age THEN 0 ELSE contract_years END"), // Set contract_years to 0 if age reaches retirement_age
+                //         'team_id' => DB::raw("CASE WHEN age + 1 >= retirement_age THEN 0 ELSE team_id END"), // Set team_id to 0 if age reaches retirement_age
+                //         'is_active' => DB::raw("CASE WHEN age + 1 >= retirement_age THEN 0 ELSE is_active END"), // Set is_active to 0 if age reaches retirement_age
+                //     ]);
 
                 ///lastly update the age of non active players
                 DB::table('players')
