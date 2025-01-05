690395862f980b250324bb17f5d461115eda4bfc
Braindead <56583469+Jxhn-Canxii@users.noreply.github.com>
2025-01-05 10:57:19 +0800

    Update Formatter.js

--- resources/js/Utility/Formatter.js
+++ resources/js/Utility/Formatter.js
@@ -311,7 +311,7 @@ export const playerFormatter = (name) => {
     const surName = nameParts[nameParts.length - 1];
 
     // Define a maximum length for the surname
-    const maxnameLength = 13; // You can adjust this value as needed
+    const maxnameLength = 7; // You can adjust this value as needed
 
     // Check if the name is too long
     if (name.length > maxnameLength) {
